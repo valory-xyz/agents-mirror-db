50397f2c218f410c6b87ba640939e6b9b6ba03ad
axatbhardwaj <axatbhardwaj@outlook.com>
2025-01-09 00:27:13 +0530

    no working

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from .db.database import init_db
 from .api import endpoints
+import time
 
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
+    time.sleep(20)
     init_db()
 
 app.include_router(endpoints.router)
